0916ebd8ffd652480ca7683d8ee789270881c607
TheTallestHobbit <site.thetallesthobbit@gmail.com>
2024-02-29 16:55:40 -0600

    new file:   .firebaserc 	new file:   .github/workflows/firebase-hosting-merge.yml 	new file:   .github/workflows/firebase-hosting-pull-request.yml 	modified:   android/app/build.gradle 	new file:   android/app/google-services.json 	new file:   firebase.json 	new file:   ios/Runner/GoogleService-Info.plist 	new file:   ios/firebase_app_id_file.json 	new file:   lib/firebase_options.dart 	modified:   lib/main.dart 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	new file:   macos/Runner/GoogleService-Info.plist 	new file:   macos/firebase_app_id_file.json 	new file:   public/index.html 	modified:   pubspec.lock 	modified:   pubspec.yaml 	modified:   windows/flutter/generated_plugin_registrant.cc 	modified:   windows/flutter/generated_plugins.cmake

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
+import 'package:firebase_auth/firebase_auth.dart'; 
+import 'package:firebase_core/firebase_core.dart'; 
+import 'package:cloud_firestore/cloud_firestore.dart'; 
+import 'package:thetallesthobbit/firebase_options.dart'; 
 
-void main() {
-  runApp(const MyApp());
-}
+Future<void> main() async { 
+
+  WidgetsFlutterBinding.ensureInitialized(); 
+
+  await Firebase.initializeApp( 
+    options: DefaultFirebaseOptions.currentPlatform, 
+  );  
+  runApp(const MyApp()); 
+} 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
